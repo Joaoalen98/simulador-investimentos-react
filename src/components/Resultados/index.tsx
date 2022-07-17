fe7ad7de0e54ea0870e191f650c2b2adf4c6780a
João Alencar <joao.alencar1497@gmail.com>
2022-07-17 18:31:24 -0300

    organização do código

--- src/components/Resultados/index.tsx
+++ src/components/Resultados/index.tsx
@@ -1,6 +1,8 @@
 import './Resultados.scss';
+
 import ResultadosSimulacao from '../../interfaces/ResultadosSimulacao';
 import formatarReal from '../../functions/formatarReal';
+import { useRef } from 'react';
 
 function Resultados ({ 
     patrimonioTotal,
@@ -12,18 +14,22 @@ function Resultados ({
     return (
         <article className="resultados">
             <h2 className="resultados__titulo">Resultados da simulação:</h2>
-            <p className="resultados__informacao">
-                Patrimônio Total: <span>{formatarReal(patrimonioTotal)}</span>
-            </p>
-            <p className="resultados__informacao">
-                Valor Investido: <span>{formatarReal(valorInvestido)}</span>
-            </p>
-            <p className="resultados__informacao">
-                Taxa Total Recebida: <span>{formatarReal(lucroTotal)}</span>
-            </p>
-            <p className="resultados__informacao">
-                Total Aportes: <span>{formatarReal(aportesMensaisTotais)}</span>
-            </p>
+            <div 
+                className='resultados__informacao'
+            >
+                <p>
+                    Patrimônio Total: <span>{formatarReal(patrimonioTotal)}</span>
+                </p>
+                <p>
+                    Valor Investido: <span>{formatarReal(valorInvestido)}</span>
+                </p>
+                <p>
+                    Taxa Total Recebida: <span>{formatarReal(lucroTotal)}</span>
+                </p>
+                <p>
+                    Total Aportes: <span>{formatarReal(aportesMensaisTotais)}</span>
+                </p>
+            </div>
         </article>
     );
 }
